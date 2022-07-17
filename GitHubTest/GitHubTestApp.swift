e2ea8133000197a052a567576c7ad6b9a562cb46
kimj0bs <trismegistos01237@gmail.com>
2022-07-18 08:52:22 +0900

    Commit Test

--- GitHubTest/GitHubTestApp.swift
+++ GitHubTest/GitHubTestApp.swift
@@ -15,6 +15,7 @@ struct GitHubTestApp: App {
         WindowGroup {
             ContentView()
                 .environment(\.managedObjectContext, persistenceController.container.viewContext)
+            
         }
     }
 }
